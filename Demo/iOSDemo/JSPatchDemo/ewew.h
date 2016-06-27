ff612b8abf9c7f11a737b0ed5ecbae4c57672471
余强 <yuqiang@yuqiangdeMacBook-Air.local>
2016-06-27 17:56:28 +0800

    pffff

---
+++ Demo/iOSDemo/JSPatchDemo/ewew.h
@@ -0,0 +1,13 @@
+//
+//  ewew.h
+//  JSPatchDemo
+//
+//  Created by 余强 on 16/6/27.
+//  Copyright © 2016年 bang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ewew : NSObject
+
+@end
---
+++ Demo/iOSDemo/JSPatchDemo/ewew.m
@@ -0,0 +1,13 @@
+//
+//  ewew.m
+//  JSPatchDemo
+//
+//  Created by 余强 on 16/6/27.
+//  Copyright © 2016年 bang. All rights reserved.
+//
+
+#import "ewew.h"
+
+@implementation ewew
+
+@end
